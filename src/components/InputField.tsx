6e49cb3482d42923822dd0f2a681e06167acd3f8
kryptocodes <srivatsantb@gmail.com>
2020-12-23 12:34:21 +0530

    Added Graphql query builder
    Fixed error code and cors error
    register: added error validation
    
    Signed-off-by: kryptocodes <srivatsantb@gmail.com>

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -9,16 +9,19 @@ import React, { InputHTMLAttributes } from "react";
 
 type InputFieldProps = InputHTMLAttributes<HTMLInputElement> & {
   label: string;
-  placeholder: string;
   name: string;
 };
 
-export const InputField: React.FC<InputFieldProps> = (props) => {
+export const InputField: React.FC<InputFieldProps> = ({
+  label,
+  size: _,
+  ...props
+}) => {
   const [field, { error }] = useField(props);
   return (
     <FormControl isInvalid={!!error}>
-      <FormLabel htmlFor={field.name}>{props.label}</FormLabel>
-      <Input {...field} {...props} id={field.name} placeholder={props.placeholder} />
+      <FormLabel htmlFor={field.name}>{label}</FormLabel>
+      <Input {...field} {...props} id={field.name} />
       {error ? <FormErrorMessage>{error}</FormErrorMessage> : null}
     </FormControl>
   );
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,18 @@
 import { ChakraProvider, ColorModeProvider } from '@chakra-ui/react'
+import { Provider,createClient  } from 'urql';
+
+const client = createClient({
+  url: "http://localhost:4000/graphql",
+  fetchOptions: {
+    credentials: "include",
+  },
+});
 
 import theme from '../theme'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <Provider value={client}>
     <ChakraProvider resetCSS theme={theme}>
       <ColorModeProvider
         options={{
@@ -13,6 +22,7 @@ function MyApp({ Component, pageProps }) {
         <Component {...pageProps} />
       </ColorModeProvider>
     </ChakraProvider>
+    </Provider>
   )
 }
 
